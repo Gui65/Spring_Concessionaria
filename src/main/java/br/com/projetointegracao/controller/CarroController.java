c5fe9ae50c7b3d200eeb797f8d66330985568b35
Gui Matos <guilhermematos851@gmail.com>
2022-02-21 14:22:32 -0300

    Implementado inclusão de carros

--- src/main/java/br/com/projetointegracao/controller/CarroController.java
+++ src/main/java/br/com/projetointegracao/controller/CarroController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import br.com.projetointegracao.model.CarroModel;
 import br.com.projetointegracao.repository.CarroRepository;
 
 @Controller
@@ -21,5 +24,16 @@ public class CarroController {
 	public String carros(Model model) {
 		model.addAttribute("listaCarros", carroRepository.findAll());
 		return "lista/carros/index";
+	}
+	
+	@GetMapping("/lista/carros/novo")
+	public String novoCarro(@ModelAttribute("carroModel") CarroModel carroModel) {
+		return "lista/carros/form";
+	}
+	
+	@PostMapping("/lista/carros/salvar")
+	public String salvarPessoa(@ModelAttribute("carroModel") CarroModel carroModel) {
+		carroRepository.save(carroModel);
+		return "redirect:/lista/carros";
 	}
 }
